c7a3a8f2268fcedc31a6201b44ea4ef681418a90
blessedjoe2000 <blessedjoe2000@yahoo.com>
2023-05-30 17:09:29 +0100

    auth api admin done

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,60 @@
+import NextAuth from "next-auth";
+import { CredentialsProvider } from "next-auth/providers";
+import { signJwtToken } from "@/app/lib/jwt";
+import bcrypt from "bcrypt";
+import Admin from "@/app/models/Admin";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {
+        email: { label: "Email", type: email },
+        password: { label: "Password", type: password },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+
+        const admin = await Admin.findOne({ email });
+
+        if (!admin) {
+          throw new Error("Invalid credentials");
+        }
+
+        const comparePassword = await bcrypt.compare(password, admin.password);
+
+        if (!comparePassword) {
+          throw new Error("Invalid credentials");
+        } else {
+          const { password, ...currentAdmin } = admin._doc;
+          const accessToken = signJwtToken(currentAdmin, { expiresIn: "7d" });
+          return {
+            ...currentAdmin,
+            accessToken,
+          };
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, admin }) {
+      if (!admin) {
+        token.accessToken = user.accessToken;
+        token._id = admin._id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.admin._id = token._id;
+        session.admin.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
